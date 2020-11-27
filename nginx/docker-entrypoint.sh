c03ea087caac559eeb05d48744d88eef0186b86d
xbexbex <petja.valkama@gmail.com>
2020-11-27 15:42:45 +0200

    Fix template generation

--- nginx/docker-entrypoint.sh
+++ nginx/docker-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 set -eu
 
-envsubst '${DATA_URL}' < /etc/nginx/conf.d/server.avoin.org.conf > /etc/nginx/conf.d/server.avoin.org.conf
+envsubst '${DATA_URL}' < /etc/nginx/conf.d/server.avoin.org.conf.template > /etc/nginx/conf.d/server.avoin.org.conf
+rm /etc/nginx/conf.d/server.avoin.org.conf.template
 
 exec "$@"
